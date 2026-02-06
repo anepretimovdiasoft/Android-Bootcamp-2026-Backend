c7c01bc5b2c6b2c423fe0c32c6a6f04e7f5a3aa3
owlli0 <balavensydbu@gmail.com>
2026-02-06 13:54:54 +0300

    feat: добавлена пагинация

--- src/main/java/ru/sicampus/bootcamp2026/service/UsersService.java
+++ src/main/java/ru/sicampus/bootcamp2026/service/UsersService.java
@@ -1,5 +1,7 @@
 package ru.sicampus.bootcamp2026.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import ru.sicampus.bootcamp2026.dto.MeetingsDTO;
 import ru.sicampus.bootcamp2026.dto.UsersDTO;
 import ru.sicampus.bootcamp2026.dto.UsersRegisterDTO;
@@ -18,4 +20,6 @@ public interface UsersService {
     void deleteUsers(Long id);
 
     UsersDTO getUsersByLogin(String login);
+
+    Page<UsersDTO> getAllUsersPaginated(Pageable pageable);
 }
