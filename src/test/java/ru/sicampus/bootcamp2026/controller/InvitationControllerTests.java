2d66f62f248d8b956d5108da01dda1a9e49f5fad
indx0 <l0rmin@yandex.com>
2026-02-07 11:05:23 +0300

    test: invitation controller tests. Service layers tests placeholders

--- src/test/java/ru/sicampus/bootcamp2026/controller/InvitationControllerTests.java
+++ src/test/java/ru/sicampus/bootcamp2026/controller/InvitationControllerTests.java
@@ -2,11 +2,23 @@ package ru.sicampus.bootcamp2026.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import ru.sicampus.bootcamp2026.dto.*;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("tests")
 @SpringBootTest
@@ -18,4 +30,139 @@ public class InvitationControllerTests {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Test
+    void createInvitation() throws Exception {
+        MeetingCreateDTO meet = new MeetingCreateDTO();
+        meet.setName("Тестовое собрание");
+        meet.setDescription("Описание тестового собрания");
+        meet.setStartTime(LocalDateTime.parse("2026-07-01T10:00:00"));
+        meet.setEndTime(LocalDateTime.parse("2026-07-01T11:00:00"));
+
+        MvcResult result = this.mockMvc.perform(
+                        post("/api/meeting/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .with(httpBasic("andrey_limasov", "1234561234"))
+                                .content(objectMapper.writeValueAsString(meet))
+                )
+                .andDo(print())
+                .andReturn();
+        String json = result.getResponse().getContentAsString();
+        MeetingDTO meetingDTO = objectMapper.readValue(json, MeetingDTO.class);
+
+        InvitationCreateDTO inv = new InvitationCreateDTO();
+        inv.setMeetingId(meetingDTO.getId());
+        inv.setEmployeeUsername("iv_ivan");
+        inv.setMessage("Приглашаю тебя на тестовое собрание");
+
+        this.mockMvc.perform(
+                        post("/api/invitation/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .with(httpBasic("andrey_limasov", "1234561234"))
+                                .content(objectMapper.writeValueAsString(inv))
+                )
+                .andDo(print())
+                .andExpect(status().isOk()
+                );
+    }
+
+    @Test
+    void createInvitationInvalid() throws Exception {
+        MeetingCreateDTO meet = new MeetingCreateDTO();
+        meet.setName("Тестовое собрание");
+        meet.setDescription("Описание тестового собрания");
+        meet.setStartTime(LocalDateTime.parse("2026-07-01T10:00:00"));
+        meet.setEndTime(LocalDateTime.parse("2026-07-01T11:00:00"));
+
+        MvcResult result = this.mockMvc.perform(
+                        post("/api/meeting/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .with(httpBasic("andrey_limasov", "1234561234"))
+                                .content(objectMapper.writeValueAsString(meet))
+                )
+                .andDo(print())
+                .andReturn();
+        String json = result.getResponse().getContentAsString();
+        MeetingDTO meetingDTO = objectMapper.readValue(json, MeetingDTO.class);
+
+        InvitationCreateDTO invInvalidUser = new InvitationCreateDTO();
+        invInvalidUser.setMeetingId(meetingDTO.getId());
+        invInvalidUser.setEmployeeUsername("zzzzzzzzzz");
+        invInvalidUser.setMessage("Приглашаю тебя на тестовое собрание");
+
+        InvitationCreateDTO invInvalidMeeting = new InvitationCreateDTO();
+        invInvalidMeeting.setMeetingId(1L);
+        invInvalidMeeting.setEmployeeUsername("iv_ivan");
+        invInvalidMeeting.setMessage("Приглашаю тебя на тестовое собрание");
+
+        this.mockMvc.perform(
+                        post("/api/invitation/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .with(httpBasic("andrey_limasov", "1234561234"))
+                                .content(objectMapper.writeValueAsString(invInvalidUser))
+                )
+                .andDo(print())
+                .andExpect(status().isNotFound()
+                );
+
+        this.mockMvc.perform(
+                        post("/api/invitation/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .with(httpBasic("andrey_limasov", "1234561234"))
+                                .content(objectMapper.writeValueAsString(invInvalidMeeting))
+                )
+                .andDo(print())
+                .andExpect(status().isNotFound()
+                );
+    }
+
+    @Test
+    void answerInvitation() throws Exception {
+        MeetingCreateDTO meet = new MeetingCreateDTO();
+        meet.setName("Тестовое собрание");
+        meet.setDescription("Описание тестового собрания");
+        meet.setStartTime(LocalDateTime.parse("2026-07-01T10:00:00"));
+        meet.setEndTime(LocalDateTime.parse("2026-07-01T11:00:00"));
+
+        MvcResult result = this.mockMvc.perform(
+                        post("/api/meeting/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .with(httpBasic("andrey_limasov", "1234561234"))
+                                .content(objectMapper.writeValueAsString(meet))
+                )
+                .andDo(print())
+                .andReturn();
+        String json = result.getResponse().getContentAsString();
+        MeetingDTO meetingDTO = objectMapper.readValue(json, MeetingDTO.class);
+
+        InvitationCreateDTO inv = new InvitationCreateDTO();
+        inv.setMeetingId(meetingDTO.getId());
+        inv.setEmployeeUsername("iv_ivan");
+        inv.setMessage("Приглашаю тебя на тестовое собрание");
+
+        MvcResult resultInv = this.mockMvc.perform(
+                        post("/api/invitation/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .with(httpBasic("andrey_limasov", "1234561234"))
+                                .content(objectMapper.writeValueAsString(inv))
+                )
+                .andDo(print())
+                .andReturn();
+        String jsonInv = resultInv.getResponse().getContentAsString();
+        InvitationDTO invitationDTO = objectMapper.readValue(jsonInv, InvitationDTO.class);
+
+        InvitationAnswerDTO answerDTO = new InvitationAnswerDTO();
+        answerDTO.setId(invitationDTO.getId());
+        answerDTO.setStatus("ACCEPTED");
+
+        this.mockMvc.perform(
+                        patch("/api/invitation/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .with(httpBasic("iv_ivan", "1234561234"))
+                                .content(objectMapper.writeValueAsString(answerDTO))
+                )
+                .andDo(print())
+                .andExpect(status().isOk()
+                );
+    }
 }
---
+++ src/test/java/ru/sicampus/bootcamp2026/service/EmployeeServiceTests.java
@@ -0,0 +1,11 @@
+package ru.sicampus.bootcamp2026.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("tests")
+@SpringBootTest
+@Transactional
+public class EmployeeServiceTests {
+}
