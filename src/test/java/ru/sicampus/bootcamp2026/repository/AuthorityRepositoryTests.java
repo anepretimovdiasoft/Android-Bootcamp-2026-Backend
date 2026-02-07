5fd0bae0254926b4d71f922ef05f965835b9f6be
indx0 <l0rmin@yandex.com>
2026-02-07 12:41:39 +0300

    fix: disable h2 console for tests test: authority repository tests

---
+++ src/test/java/ru/sicampus/bootcamp2026/repository/AuthorityRepositoryTests.java
@@ -0,0 +1,35 @@
+package ru.sicampus.bootcamp2026.repository;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import ru.sicampus.bootcamp2026.entity.Authority;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+@Transactional
+@ActiveProfiles("tests")
+public class AuthorityRepositoryTests {
+    @Autowired
+    private AuthorityRepository authorityRepository;
+
+    @Test
+    void getAuthorityById() {
+        var authority = authorityRepository.findById(1L).orElse(null);
+        assertNotNull(authority);
+        assertEquals("EMPLOYEE", authority.getAuthority());
+    }
+
+    @Test
+    void saveAuthority() {
+        Authority authority = new Authority();
+        authority.setAuthority("TEST_AUTHORITY");
+        Authority saved = authorityRepository.save(authority);
+
+        assertEquals("TEST_AUTHORITY", authorityRepository.findById(saved.getId()).orElse(null).getAuthority());
+    }
+}
