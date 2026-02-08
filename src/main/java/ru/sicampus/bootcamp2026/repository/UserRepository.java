f86ac0b7d2b933a6049e97827c88791cd797cf82
Petr Rudichev <petrrudichev@gmail.com>
2026-02-08 17:29:03 +0400

    fix: bug 4

--- src/main/java/ru/sicampus/bootcamp2026/repository/UserRepository.java
+++ src/main/java/ru/sicampus/bootcamp2026/repository/UserRepository.java
@@ -16,7 +16,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE LOWER(u.firstName) LIKE LOWER(CONCAT('%', :search, '%')) " +
             "OR LOWER(u.secondName) LIKE LOWER(CONCAT('%', :search, '%')) " +
             "OR LOWER(u.position) LIKE LOWER(CONCAT('%', :search, '%')) " +
-            "OR LOWER(u.department) LIKE LOWER(CONCAT('%', :search, '%'))")
-    Page<User> search(@Param("search") String search, Pageable pageable);
+            "OR LOWER(u.department) LIKE LOWER(CONCAT('%', :search, '%'))" +
+            "AND u.id != :userId")
+    Page<User> search(@Param("search") String search, Long userId, Pageable pageable);
 
 }
